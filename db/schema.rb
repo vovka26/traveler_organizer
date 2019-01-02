352736db1965090687de599b4ea2a090e0f2aeed
vovka26 <317110@gmail.com>
2019-01-01 21:05:14 -0500

    added more links to views

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2018_12_21_195203) do
 
   create_table "places", force: :cascade do |t|
     t.string "city"
-    
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,20 @@ Activity.destroy_all
 Place.destroy_all
 Category.destroy_all
 
-Category.create(name: "Outdoors")
-Category.create(name: "Family Friendly")
-Category.create(name: "Indoor")
-Category.create(name: "Beach")
-Category.create(name: "Tours")
+Category.find_or_create_by(name: "Outdoors")
+Category.find_or_create_by(name: "Family Friendly")
+Category.find_or_create_by(name: "Indoor")
+Category.find_or_create_by(name: "Beach")
+Category.find_or_create_by(name: "Tours")
 
-Place.create(city: "New York City")
-Place.create(city: "Sydney")
-Place.create(city: "London")
-Place.create(city: "Hong Kong")
-Place.create(city: "Paris")
+Place.find_or_create_by(city: "New York City")
+Place.find_or_create_by(city: "Sydney")
+Place.find_or_create_by(city: "London")
+Place.find_or_create_by(city: "Hong Kong")
+Place.find_or_create_by(city: "Paris")
+Place.find_or_create_by(city: "Fiji")
 
 
-Activity.create(name: "Scuba Diving", img_url: "http://www.freakingnews.com/Pictures/5/Shark-Diving.jpg")
-Activity.create(name: "Hiking", img_url: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjFh6qcksHfAhXRTd8KHffGBv4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F173388654380814346%2F&psig=AOvVaw1kTxIxWdgx4zfUBh7EOOSd&ust=1546038957190421")
-Activity.create(name: "Shopping", img_url: "https://i.pinimg.com/236x/45/9d/7d/459d7d16a5d583d8ff4170a71e94effa--shopping-meme-speech-therapy.jpg")
+Activity.find_or_create_by(name: "Scuba Diving", img_url: "http://www.freakingnews.com/Pictures/5/Shark-Diving.jpg")
+Activity.find_or_create_by(name: "Hiking", img_url: "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjFh6qcksHfAhXRTd8KHffGBv4QjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F173388654380814346%2F&psig=AOvVaw1kTxIxWdgx4zfUBh7EOOSd&ust=1546038957190421")
+Activity.find_or_create_by(name: "Shopping", img_url: "https://i.pinimg.com/236x/45/9d/7d/459d7d16a5d583d8ff4170a71e94effa--shopping-meme-speech-therapy.jpg")
