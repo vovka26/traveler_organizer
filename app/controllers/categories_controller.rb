711fe341f57d127861298bfc57d0d6c53a085460
vovka26 <317110@gmail.com>
2019-01-01 19:33:01 -0500

    added CRUD to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -9,10 +9,43 @@ class CategoriesController < ApplicationController
   def show
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
   private
 
   def find_category
     @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 
 end
