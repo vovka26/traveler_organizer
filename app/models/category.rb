4a807a2c3c7b8c0c3fbdf71ad09ed2424bf102d5
vovka26 <317110@gmail.com>
2019-01-02 18:17:05 -0500

    commit before removing forms"

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ActiveRecord::Base
   has_many :activity_places
   has_many :activities, through: :activity_categories
   has_many :places, through: :activity_places
+  accepts_nested_attributes_for :activities
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -3,4 +3,5 @@ class Place < ActiveRecord::Base
   has_many :activity_categories
   has_many :activities, through: :activity_places
   has_many :categories, through: :activity_categories
+  accepts_nested_attributes_for :activities
 end
