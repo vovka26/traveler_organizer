ead3dfc10b04e32bb7d94ddd0c406237538b7170
vovka26 <317110@gmail.com>
2019-01-01 19:57:56 -0500

    added welcome page and links in index files

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
-  root to: 'places#index'
+  root to: 'welcome#index'
   resources :categories
   resources :activities
   resources :places
+  resources :welcome, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
